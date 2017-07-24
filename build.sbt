c9a63c6f75a320c96319dbed1397c3fc775b1086
bartosz.jankiewicz <bartosz.jankiewicz@gmail.com>
2017-07-24 19:15:46 +0200

    First commit

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "warsztat-scala"
+
+version := "1.0"
+
+scalaVersion := "2.12.1"
+        
---
+++ src/main/scala/warsztat/HelloApp.scala
@@ -0,0 +1,21 @@
+package warsztat
+
+import scala.io.StdIn
+
+/**
+  * Prosta apka odpowiadająca jak papuga.
+  */
+object HelloApp extends App {
+
+  @scala.annotation.tailrec
+  def readIn(): Unit = {
+    val in = StdIn.readLine("Powiedz coś: ")
+    if(in.nonEmpty) {
+      println(s"Hello $in")
+      readIn()
+    }
+  }
+
+  readIn()
+
+}
