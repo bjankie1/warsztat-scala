5f0e4e7b82ebd8ca64482eaca27098a8b6c35903
Bartosz Jankiewicz <b.jankiewicz@ocado.com>
2017-11-29 15:12:15 +0100

    Prosta apka

--- src/test/scala/warsztat/groups/endpoints/GroupRouteTest.scala
+++ src/test/scala/warsztat/groups/endpoints/GroupRouteTest.scala
@@ -9,17 +9,20 @@ import warsztat.groups.bootstrap.{Services, SimpleDaos}
 class GroupRouteTest
     extends FlatSpec
     with Matchers
-    with ScalatestRouteTest
-    with SimpleDaos
-    with Services {
+    with ScalatestRouteTest {
 
   it should "add group" in {
-    val endpoint = new GroupRoute(groupService)
+    val services = new SimpleDaos with Services
+    val endpoint = new GroupRoute(services.groupService)
     val uuid = UUID.randomUUID().toString
     val request = Get("/groups/" + uuid)
     request ~> endpoint.routes ~> check {
       responseAs[String] shouldBe s"group: $uuid"
     }
+  }
+
+  it should "start another test" in {
+    true shouldBe true
   }
 
 }
